165e6d95ce8df2e2abd6c7fe5de7bef675039977
Ichiro Yamazaki <16699009+ichiroyamazaki@users.noreply.github.com>
2024-12-23 00:24:55 +0800

    Create MyClass.java

---
+++ MyClass.java
@@ -0,0 +1,33 @@
+public class MyClass {
+    public static void main(String args[]) {
+   
+   System.out.println("Ichiro Yamazaki");
+   System.out.println("xxxxxxxxxxx");
+   System.out.println("");
+   System.out.println("*********************** AIRLINE RESERVATION SYSTEM ****************************");
+   
+   System.out.println("");
+   System.out.println("             Seats Availability: Economy Plus = 6 Economy = 44              ");
+   System.out.println("");
+   System.out.println("               1  2  3  4  5  6  7  8  9 10 11 18 19 20 21 22 23 24         ");
+   System.out.println("      --------------------------------------------------------------------  ");
+   System.out.println("        D |    N  N  A  A  A  A  A  A  A  A  A  A  A  A  A  A  A  A  | D");
+   System.out.println("        C |    N  N  A  A  A  A  A  A  A  A  A  A  A  A  A  A  A  A  | C");
+   System.out.println("");
+   System.out.println("        A |    A  A  A  A  A  A  A  A  A  A  A  A  A  A  A  A  A  A  | A");
+   System.out.println("      ------------------------------------------------------------------_-  ");
+   System.out.println("");
+   System.out.println("      Legend: A = Available O = Occupied N = Not Applicable");
+   System.out.println("*******************************************************************************");
+   System.out.println("");
+   System.out.println("");
+   System.out.println("Reservation System Menu");
+   System.out.println(" [1] Economy Plus");
+   System.out.println(" [2] Economy");
+   System.out.println("");
+   System.out.println("Type chosen menu: 1");
+   
+   
+   
+    }
+}
